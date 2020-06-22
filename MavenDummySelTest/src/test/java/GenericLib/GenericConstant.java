d27ef956baf735502118c8ecaf6b1a77d90b65ae
bgurnani <42831184+bgurnani@users.noreply.github.com>
2020-06-22 20:05:09 +0530

    Update GenericConstant.java
    update username and password

--- MavenDummySelTest/src/test/java/GenericLib/GenericConstant.java
+++ MavenDummySelTest/src/test/java/GenericLib/GenericConstant.java
@@ -9,8 +9,8 @@ public interface GenericConstant {
 
 	
 	String excelPath=System.getProperty("user.dir")+"//Excel//POIdemo.xlsx";
-	String username="mngr260022";
-	String password="qEhUsUv";
+	String username="mngr267513";
+	String password="jUsUmyr";
 	String Url="http://demo.guru99.com/V4/";
 	String Depositxt = "Transaction details of Deposit for Account";
 	
